13cf63ab5fb3500cd08876d5daa5e0959f15667f
Mick M <mickwpmcdonnell@gmail.com>
2026-07-22 16:59:02 +0930

    Custom handles for design in scene view

---
+++ Assets/Editor/FootTargetEditor.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FootTargetUpdater))]
+[CanEditMultipleObjects]
+public class FootTargetEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        
+
+        Handles.matrix = Matrix4x4.identity;
+        Handles.color = Color.green;
+        EditorGUI.BeginChangeCheck();
+
+        foreach (Object currentTarget in targets)
+        {
+            FootTargetUpdater targetScript = (FootTargetUpdater)target;
+
+            float newRadius = Handles.RadiusHandle(
+                 targetScript.transform.rotation,
+                 targetScript.transform.position,
+                 targetScript.moveDistanceThreshold
+             );
+
+
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(targetScript, "Change move distance threshold");
+                targetScript.moveDistanceThreshold = newRadius;
+            }
+        }
+
+
+    }
+}
